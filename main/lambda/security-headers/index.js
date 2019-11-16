77ed99b87c6f26b805d7b1d2d289e893e6b68088
Jeremy Kitchen <kitchen@kitchen.io>
2019-11-15 21:12:04 -0800

    more header twiddling
    this is kinda annoying tbh

--- main/lambda/security-headers/index.js
+++ main/lambda/security-headers/index.js
@@ -5,10 +5,10 @@ exports.handler = (event, context, callback) => {
 
     headers['strict-transport-security'] = [{key: 'Strict-Transport-Security', value: 'max-age=31536042; includeSubDomains; preload'}];
     headers['x-frame-options'] = [{key: 'X-Frame-Options', value: 'DENY'}];
-    csp_string = "frame-ancestors 'none'; default-src 'self' https://www.k1chn.com/ 'unsafe-inline' https://disqus.com https://*.disqus.com https://*.disquscdn.com; style-src https://fonts.googleapis.com https://cdn.cloudflare.com https://use.fontawesome.com"
+    const csp_string = "frame-ancestors 'none'; default-src 'self' https://www.k1chn.com/ https://disqus.com https://*.disqus.com https://*.disquscdn.com; style-src https://fonts.googleapis.com https://cdn.cloudflare.com https://cdnjs.cloudflare.com https://use.fontawesome.com https://www.k1chn.com/; font-src https://fonts.gstatic.com https://use.fontawesome.com; script-src 'unsafe-inline' https://www.k1chn.com/ https://disqus.com https://*.disqus.com https://*.disquscdn.com";
     headers['content-security-policy'] = [{key: 'Content-Security-Policy', value: csp_string}];
     headers['x-xss-protection'] = [{key: 'X-XSS-Protection', value: '1; mode=block'}];
-    headers['x-content-type-options'] = [{key: 'X-Content-Type-Options', value: 'nosniff'}]
+    headers['x-content-type-options'] = [{key: 'X-Content-Type-Options', value: 'nosniff'}];
 
     callback(null, response);
 };
