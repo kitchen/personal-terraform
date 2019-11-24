e3d337caa558830f8e337d24edaab577ee9d43a2
Jeremy Kitchen <kitchen@kitchen.io>
2019-11-24 15:39:55 -0800

    set up a load balancer for vault.kitchen.horse
    I'm not sure I like this model.
    I like the forwarding rule -> http/https proxy -> backends (by rules)
    thing, but I think the mapping of proxy -> rule -> backend should be an
    external resource so I can make one load balancer for everything in
    gcp-central and just attach things to it. This would save me from having
    to have a bunch of external IPs that I have to pay for, or from having
    to centralize all of my load balancer configuration

--- vault/lb.tf
+++ vault/lb.tf
@@ -22,20 +22,77 @@ resource "google_compute_backend_service" "vault-http" {
     group = google_compute_instance_group_manager.vault.instance_group
   }
 }
-## target pool
-## global address
+
+# http proxy
+resource "google_compute_target_http_proxy" "vault" {
+  name    = "vault"
+  url_map = google_compute_url_map.vault.self_link
+}
+
+# https proxy
+resource "google_compute_target_https_proxy" "vault" {
+  name             = "vault"
+  url_map          = google_compute_url_map.vault.self_link
+  ssl_certificates = [google_compute_managed_ssl_certificate.vault-kitchen-horse.self_link]
+}
+
+resource "google_compute_global_address" "vault" {
+  name = "vault"
+}
+
+resource "google_compute_global_forwarding_rule" "vault-http" {
+  name       = "vault-http"
+  target     = google_compute_target_http_proxy.vault.self_link
+  port_range = "80"
+  ip_address = google_compute_global_address.vault.address
+}
+
+resource "google_compute_global_forwarding_rule" "vault-https" {
+  name       = "vault-https"
+  target     = google_compute_target_https_proxy.vault.self_link
+  port_range = "443"
+  ip_address = google_compute_global_address.vault.address
+}
 
 
-# dns record
+# urlmap
+resource "google_compute_url_map" "vault" {
+  name            = "vault"
+  description     = "url mapping for vault"
+  default_service = google_compute_backend_service.vault-http.self_link
 
+  host_rule {
+    description  = "vault.kitchen.horse"
+    hosts        = ["vault.kitchen.horse"]
+    path_matcher = "allpaths"
+  }
+
+  path_matcher {
+    name            = "allpaths"
+    default_service = google_compute_backend_service.vault-http.self_link
+    description     = "all paths"
+  }
+}
 
 
 # ssl certificate
 resource "google_compute_managed_ssl_certificate" "vault-kitchen-horse" {
   provider = google-beta
-  name     = "vault-kitchen-horse"
+  name     = "vault-kitchen-horse-201911400"
   managed {
     domains = ["vault.kitchen.horse."]
   }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
+resource "google_dns_record_set" "kitchen-horse-keybase" {
+  # TODO: statefile
+  project      = "central-259919"
+  name         = "vault.kitchen.horse."
+  type         = "A"
+  ttl          = 300
+  managed_zone = "kitchen-horse"
+  rrdatas      = [google_compute_global_address.vault.address]
 }
