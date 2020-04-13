fe1f2665b198308a438ec3d46b24843089df1a2a
Jeremy Kitchen <kitchen@kitchen.io>
2020-04-13 00:09:30 -0700

    turn off the NAT instance because suuuuuper expensive. Meh.

--- gcp-central/vpc.tf
+++ gcp-central/vpc.tf
@@ -19,12 +19,12 @@ resource "google_compute_router" "central" {
   network = google_compute_network.central.name
 }
 
-resource "google_compute_router_nat" "central" {
-  name                               = "central"
-  nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-  router                             = google_compute_router.central.name
-}
+# resource "google_compute_router_nat" "central" {
+#   name                               = "central"
+#   nat_ip_allocate_option             = "AUTO_ONLY"
+#   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+#   router                             = google_compute_router.central.name
+# }
 
 resource "google_compute_subnetwork_iam_member" "vault-instance-central-42" {
   # TODO: statefile
