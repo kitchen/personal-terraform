51557f85ab0a7992955dc34d6a8d79f247848711
Jeremy Kitchen <kitchen@kitchen.io>
2021-03-07 13:34:32 -0800

    delete this secret
    I'm not using it anyways I don't think. If I was, well, I'm not, now. My loss.
    
    basically divesting myself of my gcp account at the moment since I don't
    use it :D

--- gcp-central/secrets.tf
+++
@@ -1,9 +0,0 @@
-resource "google_kms_key_ring" "kitchen-secrets" {
-  name     = "kitchen-secrets"
-  location = "us-central1"
-}
-
-resource "google_kms_crypto_key" "secrets-key" {
-  name     = "secrets-key"
-  key_ring = google_kms_key_ring.kitchen-secrets.self_link
-}
