a4c7644ac309e078c4fc2db60bf0f91ac5334250
Jeremy Kitchen <kitchen@kitchen.io>
2021-03-10 23:42:59 -0800

    winding down this gcp project

--- gcp-central/drone.tf
+++
@@ -1,4 +0,0 @@
-# service account and perms and such for pohara's drone runners
-resource "google_service_account" "pohara-drone" {
-  account_id = "pohara-drone"
-}
--- gcp-central/terraform-state.tf
+++
@@ -1,6 +0,0 @@
-resource "google_storage_bucket" "kitchen-terraform" {
-  name = "kitchen-terraform"
-  versioning {
-    enabled = true
-  }
-}
